df4312747f1b70e1a70b68132bc5aa2386e2da38
GetialJuan <getial.juan@correounivalle.edu.co>
2023-03-18 15:32:05 -0500

    Add Ejercicio 3

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -1,5 +1,36 @@
-;Jose Luis Hincapie Bucheli
-;Sebatian Idrobo Avirama
-;Juan Sebastian Getial Getial
+#lang eopl
 
-;Insertar código aquí :)
+; Estudiantes:
+; Jose Luis Hincapie Bucheli - 2125340
+; Sebatian Idrobo Avirama - 2122637
+; Juan Sebastian Getial Getial - 2124644
+
+; Procedimientos:
+
+;; list-set :
+;; Proposito:
+;; L x n x X x P -> L' : Procedimiento que ingresa el elemento X en la
+;; posicion ingresada n (indexando desde cero) solo si el elemento
+;; original de la lista cumple con el predicado P.
+;;
+;; <lista> := ()
+;;         := (<valor-de-scheme> <lista>)
+
+(define list-set (
+    lambda (L n X P) 
+        [cond
+            [(eqv? n 0)
+                (if (P (car L))
+                    (cons X (cdr L))
+                    (cons (car L) (cdr L))
+                )
+            ]
+            [else
+                (cons (car L) (list-set (cdr L) (- n 1) X P))
+            ]
+        ]
+))
+
+;; Pruebas
+(list-set '(5 8 7 6) 2 '(1 2) odd?)
+(list-set '(5 8 7 6) 2 '(1 2) even?)
