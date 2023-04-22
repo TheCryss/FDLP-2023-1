d4ce71753f4cc1daf01999efa315624848bf0971
Seb0927 <sebastianidrobo@hotmail.com>
2023-04-22 11:41:56 -0500

    Se me quedaron los ejemplos :( - UNPARSEBNF ya tiene las pruebas con 'entrada3' y 'entrada4'

--- Taller2/ejercicio2.rkt
+++ Taller2/ejercicio2.rkt
@@ -8,33 +8,11 @@
 
 ;;; 2. Funciones Parse y Unparse
 
-;Función Parse
-    ;; parser-aux :
-    ;; Proposito: and-l -> and
-    ;; Procedimiento que dada una lista con la representación
-    ;; concreta de una instancia and-list, construye el árbol de sintáxis 
-    ;; abstracta basado en listas.
-    (define parser-aux
-        (lambda (fnc-l)
-            [cond
-                [(null? fnc-l)
-                    fnc-l
-                ]
-                [(eqv? (car fnc-l) 'and)
-                    (and-list (parser-aux (cadr fnc-l)))
-                ]
-                [(eqv? (caar fnc-l) 'or)
-                    (cons (or-list (cadar fnc-l)) (parser-aux (cdr fnc-l)))
-                ]
-            ]
-        )
-    )
-
 ;; PARSEBNF :
 ;; Proposito: fnc-l -> fnc
 ;; Procedimiento que dada una lista con la representación
-;; concreta de una instancia SAT (fnc), construye el árbol de sintáxis 
-;; abstracta basado en listas.
+;; concreta de una instancia 'SAT' (fnc), 'and-list' o 'or-list', 
+;; construye el árbol de sintáxis abstracta basado en listas.
 
 (define PARSEBNF
   (lambda (fnc)
@@ -53,18 +31,6 @@
        ]
       ]))
 
-
-;(define PARSEBNF
-;    (lambda (fnc)
-;        (let
-;            (
-;                [fnc-l (caddr fnc)]
-;                [num-vars (cadr fnc)]
-;            )
-;            (fnc-list num-vars (parser-aux fnc-l))
-;        )
-;    )
-;)
 
 ;; Ejemplos
 (define entrada1 (list 'fnc 2 (list 'and (list (list 'or (list 1 2)) (list 'or (list -1))))))
@@ -90,8 +56,8 @@
 ;; UNPARSEBNF :
 ;; Proposito: tree -> fnc
 ;; Procedimiento que dado un árbol de sintáxis abstracta
-;; de una instancia SAT (fnc), entregue la representación concreta basada
-;; en listas.
+;; de una instancia 'SAT' (fnc), 'and-list' o 'or-list', entregue 
+;; la representación concreta basada en listas.
 (define UNPARSEBNF
     (lambda (tree)
         [cond
@@ -99,8 +65,11 @@
                 tree
             ]
             [(eqv? (car tree) 'FNC)
+               ;(list 'fnc (fnc-list->var tree) (list 'and (UNPARSEBNF (fnc-list->clausulas tree))))
                 (list 'fnc (fnc-list->var tree) (list 'and (UNPARSEBNF (fnc-list->clausulas tree))))
             ]
+            [(eqv? (car tree) 'AND)
+                 (list 'and (UNPARSEBNF (fnc-list->clausulas tree)))]
             [(eqv? (caar tree) 'OR)
                 (cons (list 'or (or-list->varlist (car tree))) (UNPARSEBNF (cdr tree)))
             ]
@@ -111,3 +80,5 @@
 ;; Ejemplos
 (UNPARSEBNF (PARSEBNF entrada1))
 (UNPARSEBNF (PARSEBNF entrada2))
+(UNPARSEBNF (PARSEBNF entrada3))
+(UNPARSEBNF (PARSEBNF entrada4))
