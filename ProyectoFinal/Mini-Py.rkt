d97aaaf7d434d754bba621bc4c128fd6e408375f
GetialJuan <getial.juan@correounivalle.edu.co>
2023-06-10 18:23:20 -0500

    Update crear-tupla and crear-lista

--- ProyectoFinal/Mini-Py.rkt
+++ ProyectoFinal/Mini-Py.rkt
@@ -269,7 +269,7 @@
     ;Primitivas sobre listas
     (unary-primitive-list ("vacio?") is-null-primitive-list)
     (unary-primitive-list ("vacio") null-primitive-list)
-    (expression ("crear-lista" "(" expression "," expr-lista ")") make-lista-exp)
+    (expression ("crear-lista" "(" expression "," expression ")") make-lista-exp)
     (unary-primitive-list ("lista?") is-lista-primitive)
     (unary-primitive-list ("cabeza") car-primitive-list)
     (unary-primitive-list ("cola") cdr-primitive-list)
@@ -282,7 +282,7 @@
     ;Primitivas sobre tuplas
     (unary-primitive-tuple ("vacio-tupla?") is-null-primitive-tuple)
     (unary-primitive-tuple ("vacio-tupla") null-primitive-tuple)
-    (expression ("crear-tupla" "(" expression "," expr-tupla ")") make-tupla-exp)
+    (expression ("crear-tupla" "(" expression "," expression ")") make-tupla-exp)
     (unary-primitive-tuple ("tupla?") is-tuple-primitive)
     (unary-primitive-tuple ("cabeza-tupla") car-primitive-tuple)
     (unary-primitive-tuple ("cola-tupla") cdr-primitive-tuple)
@@ -399,13 +399,13 @@
       (expr-lista-exp (expr-lista) (eval-expr-lista expr-lista env))
       (make-lista-exp (exp expr-l) (eval-make-lista 
                                     (eval-expression exp env)
-                                    (eval-expr-lista expr-l env)))
+                                    (eval-expression expr-l env)))
       
       ; Tupla
       (expr-tupla-exp (expr-tupla) (eval-expr-tupla expr-tupla env))
       (make-tupla-exp (exp expr-t) (eval-make-tupla
                                     (eval-expression exp env)
-                                    (eval-expr-tupla expr-t env)))
+                                    (eval-expression expr-t env)))
 
       ;Registro
       (expr-registro-exp (expr-registro) (eval-expr-registro expr-registro env))
